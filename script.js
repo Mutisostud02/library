32032ab72fbfaba453ae0b6b253f55dddf25cee9
Mutisostud02 <mutiso8395@gmail.com>
2023-03-29 22:49:59 +0300

    Redeclared variables inside the event listener. Added code to take the book details. Added code to add the book details to myLibrary array. Initialised a div with class of book that has been set as grid of books class in the css file. Added list items to the book div and styled them. Added two buttons to the list items. A button to change the read status of a book. A button to delete a book from the page. Added the list items to the book and appended the book div to the books div. Cleared the input fields after pushing new books to the page.

--- script.js
+++ script.js
@@ -1,46 +1,105 @@
-let myLibrary = [{"Book-title":"The little mermaid",
-"Book-author":"Billy Chapman",
-Pages:200,
-}];
+let myLibrary = [];
+
+
+let bookDetails = {};
+const addBook = document.querySelector('[name="addBook"]');
 
 
 
-function Book () {
-    //constructor
-}
-let books = document.querySelector('.books');
-const addBook = document.querySelector('[name="addBook"]')
-let bookTitle = document.querySelector('#book-title');
-let bookAuthor = document.querySelector('#book-author');
-let pages = document.querySelector('#pages');
-let finishedReading;
-let book = {};
-let radio = document.getElementsByName('yes-no');
 
-function addBookToLibrary () {
-   
-   book["book-title"] = bookTitle.value;
-   book["book-author"] = bookAuthor.value;
-   book["pages"] = pages.value;
-  
-for(let i = 0;i < radio.length;i++){
-   if (radio[i].checked) {
-     book["finishedReading"] = radio[i].value;
-   }
-}
 
-   myLibrary.push(book);
-   return console.log(book,myLibrary);
-
-   
-}
 addBook.addEventListener('submit',function (event){
-    this.event = addBookToLibrary();
     event.preventDefault();
+    const books = document.querySelector('.books');    
+    const bookTitle = document.querySelector('#book-title');
+    const bookAuthor = document.querySelector('#book-author');
+    const pages = document.querySelector('#pages');
+
+    bookDetails.bookTitle =  bookTitle.value;
+    bookDetails.bookAuthor = bookAuthor.value;
+    bookDetails.pages = pages.value;
+    bookDetails.status;
+    
+    myLibrary.push(bookDetails);
+    const book = document.createElement('div');
+    book.classList.add('book');
+    book.setAttribute('style','color:#1d4ed8;font-weight:bold')
+
+    
+    const uList = document.createElement('ul');
+    uList.setAttribute('style','padding:10px;');
+    const item1 = document.createElement('li');
+    item1.setAttribute('style','list-style-type:none');
+    uList.appendChild(item1);
+    const item2 = document.createElement('li');
+    item2.setAttribute('style','list-style-type:none');
+    uList.appendChild(item2);
+    const item3 = document.createElement('li');
+    item3.setAttribute('style','list-style-type:none');
+    uList.appendChild(item3);
+    const item4 = document.createElement('li');
+    item4.setAttribute('style','list-style-type:none;margin-bottom:10px;');
+    uList.appendChild(item4);
+    const item5 = document.createElement('li');
+    item5.setAttribute('style','list-style-type:none;');
+    uList.appendChild(item5);
+    const readStatus = document.createElement('button');
+    readStatus.setAttribute('style','padding:2px;height:30px;width:70px;color:white;background-color:orange;');
+    readStatus.textContent = 'PENDING';
+    readStatus.setAttribute('type','button');
+    readStatus.addEventListener('click',function(){
+        if(readStatus.textContent=="PENDING"){
+            readStatus.textContent="FINISHED";
+            readStatus.setAttribute('style','padding:2px;height:30px;width:70px;background-color:green;color:white;');
+        }
+        else if(readStatus.textContent=="FINISHED"){
+            readStatus.textContent="PENDING";
+            readStatus.setAttribute('style','padding:2px;height:30px;width:70px;background-color:orange;color:white;');
+        }
+    });
+ 
+    
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'DELETE';
+    deleteBtn.setAttribute('style','padding:2px;height:30px;width:70px;color:red;position:relative;bottom:0;float:center;left:20%');
+    
+    deleteBtn.addEventListener('mouseover',() => {
+        deleteBtn.setAttribute('style','background-color:red;color:white;padding:2px;height:30px;width:70px;position:relative;bottom:0;float:center;left:20%');
+    });
+
+    deleteBtn.addEventListener('mouseleave',() => {
+        deleteBtn.setAttribute('style','padding:2px;height:30px;width:70px;color:red;position:relative;bottom:0;float:center;left:20%;')
+    });
+
+    deleteBtn.addEventListener('click',function (){
+        book.textContent = "";
+        book.classList.remove('book');
+        books.appendChild(book);
+        
+    });
+
+    item1.textContent = `* Book-Title: ${bookTitle.value}`;
+    item2.textContent = `* Book-Author: ${bookAuthor.value}`
+    item3.textContent = `* Pages: ${pages.value}`;
+    item4.textContent = `* Status: `;
+    item4.appendChild(readStatus);
+    item5.appendChild(deleteBtn);
+
+ 
+
+    book.appendChild(uList);
+
+
+    books.appendChild(book); 
+        
+    
+    bookTitle.value = "";
+    bookAuthor.value = "";
+    pages.value = "";
+
+
+       
 }
 );
 
-for(let i=0;i<myLibrary;i++){
-    console.log("meee");
-
-}
